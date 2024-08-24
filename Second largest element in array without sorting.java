9d1d880cc4b92bcad89c6f2c44e327e934481381
Prashant Kumar Sah <143531477+Prashant782@users.noreply.github.com>
2024-08-25 01:28:51 +0530

    Add files via upload

---
+++ Second largest element in array without sorting.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int print2largest(int[] arr) {
+        // Code Here
+            int largest = Integer.MIN_VALUE;
+            int secondLargest = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            } else if (arr[i] > secondLargest && arr[i] != largest) {
+                secondLargest = arr[i];
+            }
+        }
+
+        if (secondLargest == Integer.MIN_VALUE) {
+            return -1;
+        } else {
+            return secondLargest;
+        }
+    }
+}
